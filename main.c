e1fd3791db22dc130051eb442e1da66073c590d3
aydinterzi7 <aydinterzi7@gmail.com>
2021-04-04 23:42:44 +0300

    Structlar eklendi

--- main.c
+++ main.c
@@ -3,9 +3,22 @@
 #include <time.h>
 #include <string.h>
 #include "fonksiyonlar.h"
-//struct not
-//struct ögrenci
+typedef struct{
+	int vizeNotu;
+	int finalNotu;
+	int odevNotu;
+	float ortalamaNot;
+}not;
+
+typedef struct{
+	char ogrNo[10];
+	char ogrAd[10];
+	char ogrSoyad[10];
+	char ogrMail[20];
+	char ogrTelNo[15];
+	not notlar;
+}ogrenci;
 int main (void){
-	// ana fonksiyonlarýn gövdesi..
+	
 	return 0;
 }
